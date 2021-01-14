951b775900c97b8a61c69bee69dcb7ba736bc438
yeseul106 <cho898989@naver.com>
2021-01-14 23:28:04 +0900

    Change file location

---
+++ BaekJoon_algorithm_1744/BaekJoon_algorithm_1744/Main_1744.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#pragma warning (disable:4996)
+
+using namespace std;
+
+int main() {
+	int N;
+	scanf("%d", &N);
+
+	int arr[10000];
+	for (int i = 0; i < N; i++) {
+		int tmp;
+		scanf("%d", &tmp);
+		arr[i] = tmp;
+	}
+	sort(arr[0],arr[N-1]+1);
+
+	for (int i = N-1; i >0; i++) {
+		if(a)
+		arr[i]
+	}
+}
