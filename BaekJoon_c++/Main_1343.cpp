ea78c52ea1d97748decb8f324211bf704e4fee49
yeseul106 <cho898989@naver.com>
2022-04-03 01:14:29 +0900

    백준 알고리즘_1343 폴리오미노

--- BaekJoon_c++/Main_1343.cpp
+++ BaekJoon_c++/Main_1343.cpp
@@ -5,28 +5,78 @@
 using namespace std;
 
 int main() {
-	string answer;
+	string answer="";
 	string input;
 
 	ios_base::sync_with_stdio(0);
 
 	getline(cin, input);
+	//cout << input << endl;
 
 	int X_len = 0;
-	bool flag = false; //X가 시작되는지 확인하기 위해
+	bool flag = false; // X가 시작되는지 확인하기 위해
 	
 	for (int i = 0; i < input.length(); i++) {
+
 		if (input[i] == 'X') {
-			if (!flag) // X가 시작
+			if (!flag) { // X가 시작
 				flag = true;
+			}
 			X_len++;
+			//cout << "X_len : " << X_len << endl;
 		}
 		else if (input[i] == '.') {
 			if (flag) { // X가 끝
 				flag = false;
-				
-				while(X_len)
+
+				// 4로 먼저 나눠보기
+				int AAAA_cnt = X_len / 4;
+				X_len = X_len % 4;
+
+				if (AAAA_cnt > 0) {
+					answer.append(AAAA_cnt * 4, 'A');
+				}
+
+				int BB_cnt = X_len / 2;
+				X_len = X_len % 2;
+
+				if (X_len > 0) { // 덮을 수 없는 경우
+					cout << "-1" << endl;
+					return 0;
+				}
+				else {
+					answer.append(BB_cnt * 2, 'B');
+				}
+				answer.append(".");
+			}
+			else {
+				answer.append(".");
 			}
 		}
+
+		if (i == input.length()-1) {
+			// 4로 먼저 나눠보기
+			int AAAA_cnt = X_len / 4;
+			X_len = X_len % 4;
+
+			if (AAAA_cnt > 0) {
+				answer.append(AAAA_cnt * 4, 'A');
+			}
+
+			int BB_cnt = X_len / 2;
+			X_len = X_len % 2;
+
+			if (X_len > 0) { // 덮을 수 없는 경우
+				cout << "-1" << endl;
+				return 0;
+			}
+			else {
+				answer.append(BB_cnt * 2, 'B');
+			}
+		}
+
+		
 	}
+
+	cout << answer << endl;
 }
