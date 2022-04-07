7cfaf5582086c37183d98b3ac832f3aaab55ae8c
yeseul106 <cho898989@naver.com>
2022-04-07 23:35:11 +0900

    백준 알고리즘_10162 전자레인지

--- BaekJoon_c++/Main_10162.cpp
+++ BaekJoon_c++/Main_10162.cpp
@@ -1,21 +1,32 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-	// A : 300초, B : 60초, C : 10초
-	int a, b, c = 0;
-	int t;
-	cin >> t;
+	int T;
+	cin >> T;
 
-	if (t % 10 != 0) cout << "-1";
-	else {
-		a = t / 300;
-		b = (t % 300) / 60;
-		c = ((t % 300) % 60) / 10;
-		cout << a << " " << b << " " << c;
+	// 분, 초로 바꾸기
+	int minute = T / 60;
+	int second = T % 60;
+
+	// A B C 버튼 클릭 횟수 저장
+	int A_cnt = minute / 5;
+
+	// 5로 나눈 나머지는 1분 단위이기 때문에
+	int B_cnt = minute % 5;
+
+	int C_cnt = second / 10;
+	second = second % 10;
+
+	if (second != 0) {
+		// 시간을 정확히 맞출 수 없는 경우
+		cout << "-1" << endl;
 	}
-
-	return 0;
-
+	else {
+		cout << A_cnt << " " << B_cnt << " " << C_cnt << endl;
+	}
 }
