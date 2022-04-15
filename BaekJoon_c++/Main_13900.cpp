b751b1cee08427127d131586c1bb36ccf3eabc9e
yeseul106 <cho898989@naver.com>
2022-04-15 16:24:02 +0900

    백준 알고리즘_13900 순서쌍의 곱의 합

---
+++ BaekJoon_c++/Main_13900.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	vector<int> vec;
+
+	for (int i = 0; i < N; i++) {
+
+	}
+ }
