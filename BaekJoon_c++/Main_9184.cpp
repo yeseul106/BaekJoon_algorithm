6cd6861dd3b8c68e0fe742ccdcdf20771385a277
yeseul106 <cho898989@naver.com>
2022-06-27 17:28:54 +0900

    백준 알고리즘_9184 신나는 함수 실행

---
+++ BaekJoon_c++/Main_9184.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int dp[21][21][21] = { 0 };
+int a, b, c;
+
+int w(int a, int b, int c) {
+	if (a <= 0 || b <= 0 || c <= 0)
+		return 1;
+	else if (a > 20 || b > 20 || c > 20)
+		return w(20, 20, 20);
+	else if (a < b && b < c) {
+		if (dp[a][b][c] != 0)
+			return dp[a][b][c];
+		else
+			return dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+	}
+	else {
+		if (dp[a][b][c] != 0)
+			return dp[a][b][c];
+		else
+			return dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+	}
+}
+
+
+int main() {
+
+	while (1) {
+		cin >> a >> b >> c;
+		if (a == -1 && b == -1 && c == -1)
+			break;
+		cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << endl;
+	}
+
+}
